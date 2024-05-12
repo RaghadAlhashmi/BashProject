0ae356502e1d89eb9da9c34de6ffc7f34faeecb3
LEENALansari <143925451+LEENALansari@users.noreply.github.com>
2024-05-12 23:02:17 +0300

    Update ParkingLot.sh

--- ParkingLot.sh
+++ ParkingLot.sh
@@ -8,8 +8,18 @@ declare -a available_spots=($(seq 1 $total_spots))
 ticket_counter=0
 
 # Function for making ticket IDs:
-ticket_id() {
+generate_ticket_id() {
   ticket_counter=$((ticket_counter + 1))
   echo "Ticket$ticket_counter"
 }
-
+park_car() {
+    car=$1
+    ticked_id=$(generate_ticket_id)
+    parked_cars[$ticket_id]=$car
+    time_parked[$ticket_id]=$(date +%s)
+    occupied_spots=$((occupied_spots + 1))
+    available_spots=(${availble_spots[@]/$occupied_Spots)
+    echo "Parked car: $car"
+    echo "Ticket ID: $ticket_id"
+}
+    
